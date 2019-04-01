fb6a804fabab1b124f1774609b7a55e825fee0a3
dannertech <danner@danner.tech>
2019-04-01 01:16:50 -0400

    updated podfile and integrated Firebase authentication

--- Podfile
+++ Podfile
@@ -6,9 +6,9 @@ target 'PowerupiOS' do
   use_frameworks!
 
   # Pods for PowerupiOS
+	pod 'Firebase/Core'
 	pod 'Firebase/Auth'
 	pod 'Firebase/Database'
-	pod 'Firebase/AdMob'
 
   target 'PowerupiOSTests' do
     inherit! :search_paths
---
+++ PowerupiOS/SignupViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SignupViewController.swift
+//  PowerupiOS
+//
+//  Created by Diamonique Danner on 4/1/19.
+//  Copyright © 2019 Danner Opp., LLC. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignupViewController: UIViewController {
+    @IBOutlet var emailText: UITextField!
+    @IBOutlet var passwordText: UITextField!
+    @IBOutlet var confirmPasswordText: UITextField!
+    
+    @IBAction func submit(_ sender: Any) {
+        guard let email = emailText.text else { return }
+        if passwordText.text == confirmPasswordText.text {
+            guard let password = passwordText.text else { return }
+            
+            Auth.auth().createUser(withEmail: email, password: password) {
+                user, error in
+                if error == nil && user != nil {
+                    print("user created")
+                } else {
+                    print(error?.localizedDescription)
+                }
+            }
+        }
+       
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
